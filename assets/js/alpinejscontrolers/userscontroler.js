4a61fa30fe974b5bc129303317904a74621bcd86
Iman <imanclach90@gmail.com>
2025-10-20 22:35:06 +0330

    16th commit

--- assets/js/alpinejscontrolers/userscontroler.js
+++ assets/js/alpinejscontrolers/userscontroler.js
@@ -1,18 +1,21 @@
     document.addEventListener('alpine:init', () => {
-    Alpine.data('usersData', function() {
-        return{
+    Alpine.data('usersData', () => ({
             users : [] ,
+            isLoaded : false ,
             getUsers() {
 
+                this.isLoaded = true;
                 axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
                     this.users = res.data
+                    this.isLoaded = false;
                     console.log(res);
                     
                 })
             }
             
        
-        }
-    })
+        }) 
+
+    )
     
 })
